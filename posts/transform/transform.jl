33d8fcfbd8b3363713ecf6228f16cc309172c78e
Umair Durrani <durraniu@uwindsor.ca>
2024-01-20 20:59:16 -0500

    deleted transform folder temporarily

--- posts/transform/transform.jl
+++
@@ -1,6 +0,0 @@
-# Load parquet file
-# ] add Parquet2
-using Parquet2, DataFrames
-
-path_to_first_period_file = "posts/transform/data/i80_period1.parquet"
-data_jl = DataFrame(load(path_to_first_period_file))
